35331e0de971fcf75d09283dff6390d68136eca9
kkw5240 <kkw5240@gmail.com>
2020-10-13 02:37:34 +0900

    1차 풀이

--- src/test/java/com/kwkim/programmers/kakao2018/ThanksGivingTrafficTest.java
+++ src/test/java/com/kwkim/programmers/kakao2018/ThanksGivingTrafficTest.java
@@ -1,17 +1,86 @@
 package test.java.com.kwkim.programmers.kakao2018;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ThanksGivingTrafficTest {
     @Test
     public int solution(String[] lines) {
         int answer = 0;
 
-        //lines.spl
+        List<Log> logList = new ArrayList<>();
+        int len = lines.length;
+        long[] chkPointList = new long[len * 2];
+
+        for (int i=0; i<len; i++) {
+            Log log = new Log(lines[i]);
+
+            logList.add(log);
+
+            chkPointList[i * 2] = log.getStart();
+            chkPointList[i * 2 + 1] = log.getEnd();
+
+            chkPointList = insertionSort(chkPointList);
+        }
+
+        for (Long l : chkPointList) {
+            long temp = logList.stream()
+                    .filter(log -> log.isIn(l, l+999))
+                    .count();
+            System.out.println(temp);
+
+            answer = (answer > temp) ? answer : (int) temp;
+        }
 
         return answer;
+    }
+
+    long[] insertionSort(long[] array) {
+        int len = array.length;
+
+        for (int i=1; i<len; i++) {
+            long key = array[i];
+            int j = i - 1;
+            while (j >= 0 && key < array[j]) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = key;
+        }
+
+        return array;
+    }
+
+    @Getter @Setter
+    static class Log {
+        private long start;
+        private long proc;
+        private long end;
+
+        public Log(String timeInfo) {
+            Pattern pattern = Pattern.compile("([0-9\\-]+ [0-9:.]+) ([0-9.:]+)s");
+            Matcher matcher = pattern.matcher(timeInfo);
+
+            if (matcher.matches()) {
+                this.end = Timestamp.valueOf(matcher.group(1)).getTime();
+                this.proc = (long) (Double.parseDouble(matcher.group(2)) * 1000);
+                this.start = end - proc;
+            }
+        }
+
+        public boolean isIn(long chkStart, long chkEnd) {
+            return (this.start <= chkEnd && this.end >= chkStart);
+        }
     }
 
     @Test
