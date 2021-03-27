fd6adc85a0eceb4e1ac7f8dd59d52b74d709c214
kkw5240 <kkw5240@gmail.com>
2021-03-28 00:05:28 +0900

    Programmers 스코빌 문제 1차 풀이

---
+++ src/test/java/com/kwkim/programmers/practice/ScovilleTest.java
@@ -0,0 +1,22 @@
+package test.java.com.kwkim.programmers.practice;
+
+import main.java.com.kwkim.programmers.practice.Scoville;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScovilleTest {
+    @Test
+    void doSolution() {
+        Scoville scoville = new Scoville();
+
+        assertAll(
+                () -> assertEquals(
+                        2, scoville.solution(
+                                new int[] {1, 2, 3, 9, 10, 12}, 7
+                        )
+                )
+        );
+    }
+
+}
