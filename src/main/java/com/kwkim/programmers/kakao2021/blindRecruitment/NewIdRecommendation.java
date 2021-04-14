5f7974dd1bc4261c84875f1c5cee21d017503509
kkw5240 <kkw5240@gmail.com>
2021-04-15 00:09:54 +0900

    Programmers 2021 KAKAO BLIND RECRUITMENT 신규 아이디 추천 1차 풀이

--- src/main/java/com/kwkim/programmers/kakao2021/blindRecruitment/NewIdRecommendation.java
+++ src/main/java/com/kwkim/programmers/kakao2021/blindRecruitment/NewIdRecommendation.java
@@ -3,6 +3,28 @@ package main.java.com.kwkim.programmers.kakao2021.blindRecruitment;
 public class NewIdRecommendation {
     public String solution(String new_id) {
         String answer = "";
+
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        new_id = new_id.replaceAll("^\\.|\\.$", "");
+        if (new_id.isEmpty()) {
+            new_id = "a";
+        }
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("\\.$", "");
+        }
+        StringBuilder new_idBuilder = new StringBuilder(new_id);
+        while (new_idBuilder.length() <= 2) {
+            new_idBuilder.append(new_idBuilder.charAt(new_idBuilder.length() - 1));
+        }
+        new_id = new_idBuilder.toString();
+
+
+        System.out.println(new_id);
+        answer = new_id;
+
         return answer;
     }
 }
