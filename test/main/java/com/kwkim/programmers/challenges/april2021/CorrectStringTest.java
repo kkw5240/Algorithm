be0c95c2af285821f1db8460de9b1d1f4f301f4c
kkw5240 <kkw5240@gmail.com>
2021-04-19 12:33:56 +0900

    Programmers 월간코딩챌린지 4월 Programming2 2차 풀이

---
+++ test/main/java/com/kwkim/programmers/challenges/april2021/CorrectStringTest.java
@@ -0,0 +1,32 @@
+package main.java.com.kwkim.programmers.challenges.april2021;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CorrectStringTest {
+
+    @Test
+    void solution() {
+        CorrectString correctString = new CorrectString();
+
+        assertAll(
+                () -> assertEquals(
+                        3,
+                        correctString.solution("[](){}")
+                ),
+                () -> assertEquals(
+                        2,
+                        correctString.solution("}]()[{")
+                ),
+                () -> assertEquals(
+                        0,
+                        correctString.solution("[)(]")
+                ),
+                () -> assertEquals(
+                        0,
+                        correctString.solution("}}}")
+                )
+        );
+    }
+}
---
+++ test/main/java/com/kwkim/programmers/challenges/april2021/SumAbsolutesTest.java
@@ -0,0 +1,31 @@
+package main.java.com.kwkim.programmers.challenges.april2021;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumAbsolutesTest {
+
+    @Test
+    void solution() {
+        SumAbsolutes sumAbsolutes = new SumAbsolutes();
+
+        assertAll(
+                () -> assertEquals(
+                        9,
+                        sumAbsolutes.solution(
+                                new int[]{ 4, 7, 12 },
+                                new boolean[]{ true, false, true }
+                        )
+                ),
+                () -> assertEquals(
+                        0,
+                        sumAbsolutes.solution(
+                                new int[]{ 1, 2, 3 },
+                                new boolean[]{ false, false, true }
+                        )
+                )
+        );
+
+    }
+}
