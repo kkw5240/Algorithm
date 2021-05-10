7f31dbb1617f0489675499577e97e7d37df04f21
kkw5240 <kkw5240@gmail.com>
2021-05-10 21:02:23 +0900

    Programmers 이름이 있는 동물의 아이디 1차 풀이

--- src/main/java/com/kwkim/programmers/practice/FindIdOfAnimalWithNameForAnimalShelter.sql
+++ src/main/java/com/kwkim/programmers/practice/FindIdOfAnimalWithNameForAnimalShelter.sql
@@ -1,6 +1,9 @@
 /*https://programmers.co.kr/learn/courses/30/lessons/59407*/
 -- 코드를 입력하세요
-SELECT
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID;
 /*
 이름이 있는 동물의 아이디
 문제 설명
