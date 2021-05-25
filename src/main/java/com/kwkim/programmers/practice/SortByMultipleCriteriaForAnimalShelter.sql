41aa59439b0c888f7de1b51f532cf70e55cbebc1
kkw5240 <kkw5240@gmail.com>
2021-05-26 04:00:53 +0900

    Programmers 여러 기준으로 정렬하기 1차 풀이

--- src/main/java/com/kwkim/programmers/practice/SortByMultipleCriteriaForAnimalShelter.sql
+++ src/main/java/com/kwkim/programmers/practice/SortByMultipleCriteriaForAnimalShelter.sql
@@ -1,5 +1,10 @@
 /*https://programmers.co.kr/learn/courses/30/lessons/59404*/
 -- 코드를 입력하세요
+SELECT ANIMAL_ID,
+       NAME,
+       DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
 
 /*
 여러 기준으로 정렬하기
