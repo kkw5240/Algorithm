f3d847c1ad317dc824fe81923b148258bb66d606
kkw5240 <kkw5240@gmail.com>
2022-05-25 23:49:08 +0900

    feat: leetCode two-sum 1차 풀이
    - Test Code 작성
    - 함수 작성

--- src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
+++ src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
@@ -1,9 +1,19 @@
 package com.kwkim.leetcode.problemlist.top100likedquestions;
 
 public class TwoSum {
-    public int[] run(int[] nums, int target) {
+    public int[] solution(int[] nums, int target) {
+        int[] result = new int[2];
+        int offset = 0;
+        for (int i = offset + 1; i < nums.length; i++) {
+            if (nums[offset] + nums[i] == target) {
+                result[0] = offset;
+                result[1] = i;
+            }
+            offset++;
+        }
 
+        System.out.println(result[0] + " " + result[1]);
 
-        return new int[]{};
+        return result;
     }
 }
--- src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSumTest.java
+++ src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSumTest.java
@@ -2,17 +2,18 @@ package com.kwkim.leetcode.problemlist.top100likedquestions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class TwoSumTest {
     @Test
     void twoSum() {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-
         TwoSum twoSum = new TwoSum();
 
-        assertArrayEquals(new int[]{0, 1}, twoSum.run(nums, target));
-
+        assertAll(
+                () -> assertArrayEquals(new int[]{0, 1}, twoSum.solution(new int[]{2, 7, 11, 15}, 9)),
+                () -> assertArrayEquals(new int[]{1, 2}, twoSum.solution(new int[]{3, 2, 4}, 6)),
+                () -> assertArrayEquals(new int[]{0, 1}, twoSum.solution(new int[]{3, 3}, 6))
+        );
     }
 }
