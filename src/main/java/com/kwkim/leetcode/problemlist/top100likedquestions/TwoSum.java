ed961deafd3122988507670468849e6d18b83bbc
kkw5240 <kkw5240@gmail.com>
2022-05-26 00:12:37 +0900

    fix: leetCode two-sum 2차 풀이
    - Test Code 추가 작성
    - 함수 수정

--- src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
+++ src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
@@ -3,16 +3,15 @@ package com.kwkim.leetcode.problemlist.top100likedquestions;
 public class TwoSum {
     public int[] solution(int[] nums, int target) {
         int[] result = new int[2];
-        int offset = 0;
-        for (int i = offset + 1; i < nums.length; i++) {
-            if (nums[offset] + nums[i] == target) {
-                result[0] = offset;
-                result[1] = i;
-            }
-            offset++;
-        }
 
-        System.out.println(result[0] + " " + result[1]);
+        for (int offset = 0; offset < nums.length; offset++) {
+            for (int candidate = offset + 1; candidate < nums.length; candidate++) {
+                if (nums[offset] + nums[candidate] == target) {
+                    result[0] = offset;
+                    result[1] = candidate;
+                }
+            }
+        }
 
         return result;
     }
--- src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSumTest.java
+++ src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSumTest.java
@@ -7,13 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class TwoSumTest {
     @Test
-    void twoSum() {
-        TwoSum twoSum = new TwoSum();
-
-        assertAll(
-                () -> assertArrayEquals(new int[]{0, 1}, twoSum.solution(new int[]{2, 7, 11, 15}, 9)),
-                () -> assertArrayEquals(new int[]{1, 2}, twoSum.solution(new int[]{3, 2, 4}, 6)),
-                () -> assertArrayEquals(new int[]{0, 1}, twoSum.solution(new int[]{3, 3}, 6))
-        );
+    void twoSum_1() {
+        assertArrayEquals(new int[]{0, 1}, new TwoSum().solution(new int[]{2, 7, 11, 15}, 9));
+    }
+    @Test
+    void twoSum_2() {
+        assertArrayEquals(new int[]{1, 2}, new TwoSum().solution(new int[]{3, 2, 4}, 6));
+    }
+    @Test
+    void twoSum_3() {
+        assertArrayEquals(new int[]{0, 1}, new TwoSum().solution(new int[]{3, 3}, 6));
+    }
+    @Test
+    void twoSum_4() {
+        assertArrayEquals(new int[]{0, 2}, new TwoSum().solution(new int[]{3, 2, 3}, 6));
     }
 }
