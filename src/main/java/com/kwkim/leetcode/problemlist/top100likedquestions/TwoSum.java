d28e55b6236e427acc8fb1e89ca3030df055be7f
kkw5240 <kkw5240@gmail.com>
2022-05-26 01:00:28 +0900

    refactor: leetCode two-sum 3차 풀이
    - Map을 활용한 풀이로 변경

--- src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
+++ src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/TwoSum.java
@@ -1,5 +1,8 @@
 package com.kwkim.leetcode.problemlist.top100likedquestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     private final int[] nums;
     private final int target;
@@ -10,12 +13,16 @@ public class TwoSum {
     }
 
     private int[] solution() {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
         for (int offset = 0; offset < nums.length; offset++) {
-            for (int candidate = offset + 1; candidate < nums.length; candidate++) {
-                if (nums[offset] + nums[candidate] == target) {
-                    return new int[]{offset, candidate};
-                }
+            int requiredNumber = this.target - this.nums[offset];
+
+            if (indexMap.containsKey(requiredNumber)) {
+                return new int[]{indexMap.get(requiredNumber), offset};
             }
+
+            indexMap.put(this.nums[offset], offset);
         }
 
         return null;
