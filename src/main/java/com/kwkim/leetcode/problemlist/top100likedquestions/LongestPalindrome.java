e6a87acb6a32d6fbfae16751e648662268daf74d
kkw5240 <kkw5240@gmail.com>
2022-06-11 23:42:31 +0900

    feat(leetCode): 5. Longest Palindromic Substring 구현
    - 1차 풀이 작성 중

--- src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/LongestPalindrome.java
+++ src/main/java/com/kwkim/leetcode/problemlist/top100likedquestions/LongestPalindrome.java
@@ -1,61 +1,88 @@
 package com.kwkim.leetcode.problemlist.top100likedquestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 5. Longest Palindromic Substring
  * Medium
- *
+ * <p>
  * Given a string s, return the longest palindromic substring in s.
- *
+ * <p>
  * Example 1:
  * Input: s = "babad"
  * Output: "bab"
  * Explanation: "aba" is also a valid answer.
- *
+ * <p>
  * Example 2:
  * Input: s = "cbbd"
  * Output: "bb"
- *
+ * <p>
  * Constraints:
  * 1 <= s.length <= 1000
  * s consist of only digits and English letters.
  */
 public class LongestPalindrome {
     public String solution(String s) {
+        System.out.println("[" + s + "]");
+
         if (s.length() == 1) {
             return s;
         }
 
-        String[] ss = s.split("");
+        for (int i = 0; i < s.length(); i++) {
+            String prev = getPrev(s, i);
+            String post = getPost(s, i);
 
-        List<String> prev = new ArrayList<>();
-        for (int i = 0; i < ss.length; i++) {
-            String p;
-            String c;
-            String n;
+            printTemporary(prev, post);
 
-            c = ss[i];
-            if (i == 0) {
-                p = "";
-            } else {
-                p = ss[i-1];
+            StringBuilder result = new StringBuilder();
+            int minLength = Math.min(prev.length(), post.length());
+            for (int j = 0; j < minLength; j++) {
+                if (prev.charAt(j) != post.charAt(j)) {
+                    if (prev.charAt(j) == s.charAt(i)) {
+                        result.append(prev.charAt(j));
+                        result.append(s.charAt(i));
+                    }
+
+                    if (post.charAt(j) == s.charAt(i)) {
+                        result.append(s.charAt(i));
+                        result.append(post.charAt(j));
+                    }
+
+                    break;
+                }
+
+                result.append(prev.charAt(j));
+                result.append(s.charAt(i));
+                result.append(post.charAt(j));
             }
-
-            if (i == ss.length-1) {
-                n = "";
-            } else {
-                n = ss[i+1];
-            }
-
-            if (p.equals(n)) {
-
-            } else {
-                prev.add(p);
-            }
+            System.out.println("\t" + result);
         }
 
         return null;
+    }
+
+    String getPrev(String s, int i) {
+        List<String> prev = new ArrayList<>(Arrays.asList(
+                s.substring(0, i).split("")
+        ));
+
+        Collections.reverse(prev);
+
+        return String.join("", prev);
+    }
+
+    String getPost(String s, int i) {
+        return s.substring(i + 1);
+    }
+
+
+    private void printTemporary(String prev, String post) {
+        System.out.println(prev + " - " + post);
+        System.out.println();
+        System.out.println();
     }
 }
--- src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/LongestPalindromeTest.java
+++ src/test/java/com/kwkim/leetcode/problemlist/top100likedquestions/LongestPalindromeTest.java
@@ -2,9 +2,29 @@ package com.kwkim.leetcode.problemlist.top100likedquestions;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LongestPalindromeTest {
+    @Test
+    void getPrev1() {
+        assertEquals(Arrays.asList("a", "b"), new LongestPalindrome().getPrev("babad", 2));
+    }
+    @Test
+    void getPrev2() {
+        assertEquals(List.of("b"), new LongestPalindrome().getPrev("babad", 1));
+    }
+    @Test
+    void getPost1() {
+        assertEquals(Arrays.asList("a", "d"), new LongestPalindrome().getPost("babad", 2));
+    }
+    @Test
+    void getPost2() {
+        assertEquals(List.of("b", "a", "d"), new LongestPalindrome().getPost("babad", 1));
+    }
+
     @Test
     void example1() {
         assertEquals("bab", new LongestPalindrome().solution("babad"));
